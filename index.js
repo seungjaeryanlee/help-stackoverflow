1999bfcc299074287572f0890898291adb371912
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-06-23 09:51:04 -0400

    Fixed so that console logs are still displayed

--- index.js
+++ index.js
@@ -4,5 +4,6 @@
  * @author Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
  */
 process.on('uncaughtException', function(errorMessage) {
+  console.log(errorMessage.stack);
   require('opn')('https://stackoverflow.com/search?q=' + errorMessage);
 });
